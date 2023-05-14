eba4a4adc8f6bbb871a13f11b49660d8f473679c
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-05-14 17:27:06 +0900

    Remove Get download URL, Add content

--- Assets/Scripts/Community/CommunityMain.cs
+++ Assets/Scripts/Community/CommunityMain.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using TMPro;
+
 using Firebase;
-using Firebase.Database;
 using Firebase.Auth;
+using Firebase.Database;
 using Firebase.Storage;
 using Firebase.Extensions;
 
 public class CommunityMain : MonoBehaviour
 {
     FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
     DatabaseReference reference;
     FirebaseStorage storage;
     StorageReference storageReference;
@@ -22,47 +24,54 @@ public class CommunityMain : MonoBehaviour
     ScrollRect scrollRect;
     public TMP_Text Message;
 
+    DataSnapshot posts;
+    long size; // 전체 posts 사이즈
+    long currSize = 0; // 현재 posts 페이지의 최대 사이즈
+
     void Start()
     {
         auth = FirebaseAuth.DefaultInstance;
-        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        user = auth.CurrentUser;
+
         if (user != null) {
             Message.text = "Hello, your name is " + user.DisplayName + " and your email is " + user.Email;
         }
         else {
             Message.text = "No user.";
         }
-        getImages();
-    }
-
-    void getImages()
-    {
+        
         scrollRect = GameObject.Find("Scroll View").GetComponent<ScrollRect>();
-        storage = FirebaseStorage.DefaultInstance;
-        storageReference = storage.GetReferenceFromUrl("gs://picaxer-22bea.appspot.com");
 
+        // databasereference 전체 post 가져오는 부분
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
 
-        //
-        RawImage img = scrollRect.content.GetChild(0).GetChild(0).GetComponent<RawImage>();
-        StorageReference imageRef = storageReference.Child("post").Child("test.png");
-        // Fetch the download URL
-        imageRef.GetDownloadUrlAsync().ContinueWithOnMainThread(task => {
-            if (!task.IsFaulted && !task.IsCanceled) {
-                Debug.Log("Download URL: " + task.Result); 
-                try
-                {
-                    StartCoroutine(ImageLoad(img, Convert.ToString(task.Result)));
-                }
-                catch (System.Exception ex)
-                {
-                    Debug.Log(ex);
-                    throw;
-                }
+        reference.Child("Post").GetValueAsync().ContinueWithOnMainThread(task => {
+            if (task.IsCompleted) {
+                posts = task.Result;
+                size = posts.ChildrenCount; // 추후 size는 고정으로 변경
+                Debug.Log(posts);
+                Debug.Log(size);
                 
+                PrintRawImage(); 
             }
         });
-        //
     }
+
+    void PrintRawImage() 
+    {
+        int i = 0;
+        foreach (DataSnapshot post in posts.Children)
+        {
+            if (i >= size) break;
+            RawImage img = scrollRect.content.GetChild(i).GetChild(0).GetComponent<RawImage>();
+            TMP_Text text = scrollRect.content.GetChild(i).GetChild(1).GetComponent<TMP_Text>();
+            StartCoroutine(ImageLoad(img, post.Child("imageURL").Value.ToString()));
+            text.text = post.Child("content").Value.ToString();
+            i++;
+        }
+    }
+
 
     IEnumerator ImageLoad(RawImage img, string MediaUrl)
     {
@@ -78,9 +87,25 @@ public class CommunityMain : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PrevClick()
     {
-        
+        Prev();
+    }
+
+    public void NextClick()
+    {
+        Next();
+    }
+
+    void Prev()
+    {
+        PrintRawImage();
+        currSize -= 15;
+    }
+
+    void Next()
+    {
+        PrintRawImage();
+        currSize += 15;
     }
 }
