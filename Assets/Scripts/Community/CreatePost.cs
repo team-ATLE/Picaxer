3c6c7d6dc03158836fcda54b4eb922a337ecd660
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-05-28 18:32:47 +0900

    Scrollview에 데이터 개수만큼 화면에 보이도록 수정: postCreate, CommunityMain

--- Assets/Scripts/Community/CreatePost.cs
+++ Assets/Scripts/Community/CreatePost.cs
@@ -76,7 +76,6 @@ public class CreatePost : MonoBehaviour
                 });
 
                 StartCoroutine(UploadWait());
-                SceneManager.LoadScene("CommunityMain");
             }
         });
 
@@ -85,7 +84,8 @@ public class CreatePost : MonoBehaviour
 
     IEnumerator UploadWait() 
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(7.0f);
+        SceneManager.LoadScene("CommunityMain");
     }
 
     public void UpdateImgURL(string name)
