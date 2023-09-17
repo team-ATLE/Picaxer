6a948c2be7e82d24e42b8179445489f5ce86519e
hgyuhyeon <hwgyuhyeon@gmail.com>
2023-09-17 16:34:20 +0900

    ADD: 프로필 사진 추가

--- Assets/Scripts/Main/ProfileEditor.cs
+++ Assets/Scripts/Main/ProfileEditor.cs
@@ -2,34 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
 using TMPro;
 using Firebase;
+using Firebase.Extensions;
 using Firebase.Auth;
-// using Firebase.Database;
+using Firebase.Storage;
+using static CommunityDAO;
 
 public class ProfileEditor : MonoBehaviour
 {
+    private string uid;
     public TMP_Text Email;
     public TMP_InputField Name;
     public System.Uri Photo_url;
     public TMP_Text Message;
     string res;
+    
+    string imageName;
+    
     FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+    FirebaseStorage storage;
+    StorageReference storageReference;
+    RawImage photo;
     // DatabaseReference reference;
 
     void Start()
     {
         auth = FirebaseAuth.DefaultInstance;
-        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        user = auth.CurrentUser;
         if (user != null) {
             Email.text = user.Email;
             Name.text = user.DisplayName;
             Photo_url = user.PhotoUrl;
-            string uid = user.UserId;
+            uid = user.UserId;
         }
         else {
-            Email.text = "email.com";
-            Name.text = "asdf";
+            Email.text = "";
+            Name.text = "";
+        }
+
+        CommunityDAO dao = new CommunityDAO();
+        storageReference = FirebaseStorage.DefaultInstance.GetReferenceFromUrl(dao.getReferenceURL());
+
+        photo = gameObject.GetComponentInChildren<RawImage>();
+
+        StartCoroutine(ProfileLoad(Convert.ToString(Photo_url)));
+    }
+
+    IEnumerator ProfileLoad(string MediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(MediaUrl);
+        yield return request.SendWebRequest();
+        if(request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(request.error);
+        }
+        else
+        {
+            photo.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
         }
     }
 
@@ -54,13 +90,12 @@ public class ProfileEditor : MonoBehaviour
 
     void Save(string name)
     {
-        // button onclick
-        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
         if (user != null) {
             Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile {
                 DisplayName = name,
                 PhotoUrl = Photo_url,
             };
+
             user.UpdateUserProfileAsync(profile).ContinueWith(task => {
                 if (task.IsCanceled) {
                     Debug.LogError("UpdateUserProfileAsync was canceled.");
